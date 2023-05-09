060651e1ccf84ac34e33cacead21965c58bd5060
GASADER <kajorhkaen@gmail.com>
2023-05-09 10:29:34 +0700

    Add: middleware logger

--- src/route/index.js
+++ src/route/index.js
@@ -1,10 +1,15 @@
 import UserRouter from "../users/users.router";
 import PostRouter from "../posts/posts.router";
+import logger from "../../middleware/logger";
 
 function routers(app) {
+  //เรียกใช้ middleware
+  app.use(logger)
+
   app.get("/", (req, res) => res.send("API ok kk"));
   app.use("/users", UserRouter);
   app.use("/posts", PostRouter);
+
 
   return app;
 }
