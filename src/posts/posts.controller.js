f1afeefb659052d8a4fdb6895287f275aa96e007
Songphon Yuenyaw <songphon.yy@hotmail.com>
2023-05-10 14:05:33 +0700

    feat: Implement Post Method of post resource

--- src/posts/posts.controller.js
+++ src/posts/posts.controller.js
@@ -1,8 +1,20 @@
-import { getPosts } from "./posts.service";
+import { getPosts, createPost } from "./posts.service";
 
 export const findAll = async (req, res) => {
   const posts = await getPosts();
 
   // อันนี้เขียนให้ return เป็น string ไปก่อน ต้องไปเขียน รับเป็น json มาเองจาก service นะ อย่าลืมเปลี่ยน send เป็น ่json ด้วย
   res.send(posts);
+};
+
+export const create = async (req, res) => {
+  const post = req.body;
+  let error;
+  try {
+    const newPost = await createPost(post);
+    res.status(201).json(newPost);
+  } catch (err) {
+    error = err;
+    res.status(400).send(error);
+  }
 };
--- src/posts/posts.service.js
+++ src/posts/posts.service.js
@@ -1,3 +1,10 @@
+import Post from "../models/post.schema";
+
 export async function getPosts() {
   return "This is allPosts that you want ^^";
+}
+
+export function createPost(post) {
+  const postModel = new Post(post);
+  return postModel.save();
 }
