9a5d84d0a03f6cb2376ca1c4aecb272d3c7fa20d
pkpndLiZ <phanida.lip@gmail.com>
2023-05-12 00:01:34 +0700

    fix: add username,userImage, type to post schema

--- src/models/post.schema.js
+++ src/models/post.schema.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
   userId: { type: String, required: true },
+  username: { type: String, required: true },
+  userImage: { type: String },
   imageUrl: { type: String },
+  type: {
+    type: String,
+    enum: ["Biking", "Walking", "Swimming", "Hiking", "Running"],
+  },
   duration: { type: String, type: String, required: true },
   distance: { type: Number, required: true },
   date: { type: String, required: true },
