dea6ac687e7313708a61b79a17f1772b6552bb6a
pkpndLiZ <phanida.lip@gmail.com>
2023-05-11 22:25:55 +0700

    fix: edit field in postSchema

--- src/models/post.schema.js
+++ src/models/post.schema.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  profileName: { type: String, required: true },
-  profileImage: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String },
   duration: { type: String, type: String, required: true },
   distance: { type: Number, required: true },
   date: { type: String, required: true },
