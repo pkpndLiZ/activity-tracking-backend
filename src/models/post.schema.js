67159b4d8037a8f7184363640a54957469f58523
pkpndLiZ <phanida.lip@gmail.com>
2023-05-18 02:46:29 +0700

    fix: fix backend error

--- src/models/post.schema.js
+++ src/models/post.schema.js
@@ -5,24 +5,25 @@ const durationSchema = new mongoose.Schema({
   min: { type: Number, required: true },
 });
 
-const postSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  username: { type: String, required: true },
-  userImage: { type: String },
-  imageUrl: { type: String },
-  type: {
-    type: String,
-    enum: ["Biking", "Walking", "Swimming", "Hiking", "Running"],
+const postSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    username: { type: String, required: true },
+    userImage: { type: String },
+    imageUrl: { type: String },
+    type: {
+      type: String,
+      enum: ["Biking", "Walking", "Swimming", "Hiking", "Running"],
+    },
+    distance: { type: Number, required: true },
+    duration: { type: durationSchema, required: true },
+    date: { type: String, required: true },
+    title: { type: String },
+    description: { type: String },
+    post_status: { type: Boolean, required: true },
   },
-  distance: { type: Number, required: true },
-  duration: { type: durationSchema, required: true },
-  date: { type: String, required: true },
-  title: { type: String },
-  description: { type: String },
-  //เพิ่มวันที่แก้ไขและสถานะ
-  timestamps: { type: Boolean },
-  post_status: {type:Boolean , required: true}
-});
+  { timestamps: true }
+);
 
 const Post = mongoose.model("Post", postSchema);
 
