71869804050c5fb06e9cb43b8ac22833b0cb7fd4
pkpndLiZ <phanida.lip@gmail.com>
2023-05-18 02:44:48 +0700

    feat: add middleware bodyParser and add condition

--- src/route/index.js
+++ src/route/index.js
@@ -1,11 +1,10 @@
 import UserRouter from "../users/users.router.js";
 import PostRouter from "../posts/posts.router.js";
-import logger from "../../middleware/logger.js";
-import cors from "cors";
+import logger from "../middleware/logger.js";
 
 function routers(app) {
   //เรียกใช้ middleware
-  app.use(cors());
+
   app.use(logger);
   app.get("/", (req, res) => res.send("API ok kk"));
   app.use("/users", UserRouter);
