f729ee99543a3633c0a97c447fd0721e23c014d8
pkpndLiZ <phanida.lip@gmail.com>
2023-05-21 05:52:30 +0700

    feat: adjust and testing register system

--- src/users/users.service.js
+++ src/users/users.service.js
@@ -1,43 +1,10 @@
-import { faker } from "@faker-js/faker";
 import User from "../models/user.schema.js";
-
-const users = [
-  {
-    id: faker.random.numeric(4),
-    name: "Phanida",
-  },
-  {
-    id: faker.random.numeric(4),
-    name: "สมหงิ งวย",
-  },
-  {
-    id: faker.random.numeric(4),
-    name: "ทนงทวย คงควรคอย",
-  },
-  {
-    id: faker.random.numeric(4),
-    name: faker.name.fullName(),
-  },
-  {
-    id: faker.random.numeric(4),
-    name: faker.name.fullName(),
-  },
-  {
-    id: faker.random.numeric(4),
-    name: faker.name.fullName(),
-  },
-];
 
 export async function getUsers() {
   return users;
 }
 
 export function createUser(user) {
-  // const user = {
-  //   id: faker.random.numeric(4),
-  //   name: name,
-  // };
-  // users.push(user);
   const userModel = new User(user);
   return userModel.save();
 }
