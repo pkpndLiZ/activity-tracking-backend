ddfc328de821c2bf9df65660cf3eb8ab4e4892d8
pkpndLiZ <phanida.lip@gmail.com>
2023-05-20 09:58:55 +0700

    feat: verify user token by firebase

---
+++ src/middleware/verifyUser.js
@@ -0,0 +1,26 @@
+import serviceAccount from "../../service-account.json";
+import admin from "firebase-admin";
+
+const app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default async function verifyUser(req, res, next) {
+  const rawToken = req.headers.authorization;
+
+  if (!rawToken) {
+    return res.status(401).send("require token!!");
+  }
+
+  const token = rawToken.split(" ").pop();
+  try {
+    const decodedToken = await app.auth().verifyIdToken(token);
+    req.user = {
+      id: decodedToken.uid,
+    };
+
+    next();
+  } catch (error) {
+    res.status(401).send("Invalid token");
+  }
+}
